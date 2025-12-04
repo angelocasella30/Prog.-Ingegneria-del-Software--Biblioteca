5a634a40e9ca58579ad0c45b645a039874a18305
LuisaProject <luisaproject09@gmail.com>
2025-12-04 14:00:10 +0100

    aggiunta doxygen comments in gestionelibricontroller

--- src/main/java/Classibiblioteca/Controllers/GestioneLibriController.java
+++ src/main/java/Classibiblioteca/Controllers/GestioneLibriController.java
@@ -8,6 +8,10 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
+/**
+ * Controller che gestisce operazioni su libri
+ * come la creazione, modifica,elimina dei libri
+ */
 public class GestioneLibriController {
 
     @FXML
@@ -33,36 +37,74 @@ public class GestioneLibriController {
 
     private ArchivioLibri archivioLibri;
 
+/**
+ * Metodo initData
+ * 
+ * collega archivio libri 
+ */
     // Metodo per ricevere i dati dal MainController
     public void initData(ArchivioLibri archivio) {
         this.archivioLibri = archivio;
     }
-
+/**
+ * Metodo initialize
+ * 
+ * inizializza la tabella di dati archivio libri
+ */
+    
     @FXML
     public void initialize() {
         // TODO: Collegare le colonne agli attributi della classe Libro
     }
+/**
+ * Metodo handleCerca gestisce la ricerca libro
+ * 
+ * gestisce la ricerca titolo,autore,isbn in ordine alfabetico
+ * controllo di eccezione per libro inesistente o input errato
+ */
 
     @FXML
     void handleCerca(ActionEvent event) {
         // TODO: Implementare ricerca (UC-4)
     }
-
+/**
+ * Metodo handleReset gestisce l'evento annullamento dati
+ * 
+ * gestisce lo svuotamento di campi del form libro 
+ */
+    
     @FXML
     void handleReset(ActionEvent event) {
         // TODO: Resettare filtri e mostrare tutto
     }
-
+/**
+ * Metodo handleNuvo gestisce l'evento creazione libro
+ * 
+ * gestisce form nuovo con controllo dati archivio per coincidenza
+ * isbn come eccezione
+ */
+    
     @FXML
     void handleNuovo(ActionEvent event) {
         // TODO: Aprire popup nuovo libro (UC-1)
     }
-
+/**
+ * Metodo handleModifica gestisce l'evento modifca libro
+ * 
+ * gestisce la modifica del form dettagli libro
+ */
+    
     @FXML
     void handleModifica(ActionEvent event) {
         // TODO: Aprire popup modifica libro (UC-2)
     }
-
+/**
+ * Metodo handleElimina gestisce l'evento eliminalibro
+ * 
+ * gestisce l'eliminazione di libro con attenzione di eccezione
+ * per libri in prestito
+ */
+    
     @FXML
     void handleElimina(ActionEvent event) {
         // TODO: Eliminare libro selezionato (UC-3)
